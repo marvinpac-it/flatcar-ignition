1b4d732608fcb2dbcba7ef9b7daeb9c929275e1f
Olivier Boudry <79530946+oboudry-mvp@users.noreply.github.com>
2024-04-25 16:37:12 +0200

    Update transpile.sh

--- transpile.sh
+++ transpile.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-if [ -z "$1" -o -z "$2" ]; then
-    echo "Veuillez fournir le nom du serveur et l'IP comme arguments."
-    echo 'example: $> bash transpile.sh flatcar01 192.168.77.101 > ignition.json'
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Please provide the server name and the IP address as arguments."
+    echo 'Example: $ bash transpile.sh flatcar01 192.168.77.101 > ignition.json'
     exit 1
 fi
 
-sed -e "s/HOSTNAME/$1/" -e "s/IPADDRESS/$2/" $(dirname "$0")/cl.yaml | docker run --rm -i quay.io/coreos/butane:latest
+# Determine the name of the first non-loopback interface
+non_loopback_interface=$(ip -o link show | awk '!/^[0-9]*: lo:/' | awk '{sub(/:$/, "", $2); print $2; exit}')
+
+sed -e "s/HOSTNAME/$1/" -e "s/IPADDRESS/$2/" -e "s/INTERFACE/$non_loopback_interface/" "$(dirname "$0")/cl.yaml" | docker run --rm -i quay.io/coreos/butane:latest
